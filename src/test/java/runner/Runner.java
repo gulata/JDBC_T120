293c692711ad1bd4e901810762339e8a0aebdf62
gulata <gul.ataalp@gmail.com>
2023-11-10 23:38:33 +0300

    .

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"
+
         },
         features = "src/test/resources",
         glue = {"stepDefinitions"},
